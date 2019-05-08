01a0a5a1394534a9a4dc9b9ba5ba00830d239136
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-07 22:32:46 -0600

    Add tests for instance and attributes of cruiser

---
+++ test/ship_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_ship_exists
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_ship_attributes
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+    assert_equal 3, @cruiser.health
+  end
+
+end
