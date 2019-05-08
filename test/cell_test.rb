0b1d9a2d148a0f9b360ed0c665064c025bec99c4
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-08 15:16:13 -0600

    Add test to render when a ship is hit and then sunk

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -7,8 +7,8 @@ require './lib/cell.rb'
 class CellTest < MiniTest::Test
 
   def setup
-    @cell_1 = Cell.new("B4")
-    @cell_2 = Cell.new("C3")
+    @cell_1 = Cell.new('B4')
+    @cell_2 = Cell.new('C3')
 
     @submarine = Ship.new("Submarine", 2)
     @cruiser = Ship.new("Cruiser", 3)
@@ -19,7 +19,7 @@ class CellTest < MiniTest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "B4", @cell_1.coordinate
+    assert_equal 'B4', @cell_1.coordinate
   end
 
   def test_does_cell_have_a_ship
@@ -47,19 +47,34 @@ class CellTest < MiniTest::Test
   end
 
   def test_if_it_renders
-    assert_equal ".", @cell_1.render
+    assert_equal '.', @cell_1.render
   end
 
   def test_if_cell_renders_miss_when_fired_upon
     @cell_1.fire_upon
 
-    assert_equal "M", @cell_1.render
+    assert_equal 'M', @cell_1.render
   end
 
   def test_render_and_optional_argument
     @cell_2.place_ship(@cruiser)
     assert_equal '.', @cell_2.render
-    assert_equal "S", @cell_2.render(true)
+    assert_equal 'S', @cell_2.render(true)
   end
+
+  def test_render_ship_hit_and_sunk
+    @cell_2.place_ship(@cruiser)
+    @cell_2.fire_upon
+
+    assert_equal 'H', @cell_2.render
+    refute @cruiser.sunk?
+
+    @cruiser.hit
+    @cruiser.hit
+
+    assert @cruiser.sunk?
+    assert_equal 'X', @cell_2.render
+  end
+
 
 end
