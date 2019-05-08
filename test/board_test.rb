352f434cd919614c9cf797f525c7b5a113f35e12
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-08 16:23:39 -0600

    Add test for valid cell coordinate

--- test/board_test.rb
+++ test/board_test.rb
@@ -3,6 +3,7 @@ require 'minitest/pride'
 require 'pry'
 
 require './lib/board'
+require './lib/cell'
 
 class BoardTest < MiniTest::Test
 
@@ -12,6 +13,17 @@ class BoardTest < MiniTest::Test
 
   def test_board_exists
     assert_instance_of Board, @board
+  end
+
+  def test_board_has_cells
+    # check all values are instances of cells
+    # check keys (A1)
+    # check that .cells returns a Hash
+    assert_instance_of Hash, @board.cells
+  end
+
+  def test_valid_cell_coordinates
+    assert_equal true, @board.valid_coordinate?("A1")
   end
 
 end
