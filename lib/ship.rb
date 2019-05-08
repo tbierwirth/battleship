40b8731eb464e09a5af4e992d6cee2056e013984
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-07 22:39:31 -0600

    Add method for reducing health of ship and increasing hit counter when it is hit

--- lib/ship.rb
+++ lib/ship.rb
@@ -5,10 +5,16 @@ class Ship
     @name = name
     @length = length
     @health = 3
+    @hit_counter = 0
   end
 
   def sunk?
     @health == 0
+  end
+
+  def hit
+    @health -= 1
+    @hit_counter += 1
   end
 
 end
