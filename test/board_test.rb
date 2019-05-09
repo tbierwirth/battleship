424395360c3af6a5eb5acbebc484f3943245501d
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-09 11:10:08 -0600

    Add test for ship length and valid cell placement

--- test/board_test.rb
+++ test/board_test.rb
@@ -4,11 +4,14 @@ require 'pry'
 
 require './lib/board'
 require './lib/cell'
+require './lib/ship'
 
 class BoardTest < MiniTest::Test
 
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_board_exists
@@ -17,17 +20,27 @@ class BoardTest < MiniTest::Test
 
   def test_board_has_cells
     # check all values are instances of cells
-    # check keys (A1)
-    # check that .cells returns a Hash
+    # check keys (A1) -
+    # check that .cells returns a Hash -
     assert_instance_of Hash, @board.cells
   end
 
+  def test_first_element_of_hash
+    assert_instance_of Cell, @board.cells["A1"]
+  end
+
   def test_valid_cell_coordinates
-    assert_equal true, @board.valid_coordinate?("A1")
+    assert @board.valid_coordinate?("A1")
     assert_equal true, @board.valid_coordinate?("D4")
     assert_equal false, @board.valid_coordinate?("A5")
     assert_equal false, @board.valid_coordinate?("E1")
     assert_equal false, @board.valid_coordinate?("A22")
   end
+
+  def test_ship_length_to_valid_placements
+    refute @board.valid_placement?(@cruiser, ["A1", "A2"])
+    refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"])
+  end
+
 
 end
