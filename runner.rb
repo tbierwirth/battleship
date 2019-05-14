60fdd7155bdef4c91d69460768ca7d185282e5d8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-14 14:39:17 -0600

    Add pseudocode

--- runner.rb
+++ runner.rb
@@ -1,3 +1,14 @@
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
+
+puts "Welcome to Battleship"
+puts "Enter p to Play or q to Quit"
+input = gets.chomp
+if input == 'p'
+  # Create board for computer and place ships randomly
+  # Create bordd for player and ask for coordinates to place their ships
+  # Start the game/initialize the Turn
+elsif input == 'q'
+  # Stop the game
+end
