0c9c8bc9601d46227b69a1d7d81d5c9f95b5491a
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-14 21:12:46 -0600

    Add method to generate random valid placement of a ship

--- lib/setup.rb
+++ lib/setup.rb
@@ -14,4 +14,14 @@ class Setup
     # "#{rand(65..Board_Size).chr}#{rand(1..Board_Size)}"
   end
 
+  def random_placement(ship)
+    coordinates = []
+    coordinates.push(random_coordinate)
+    until @computer.valid_placement?(ship, coordinates) do
+      coordinates.push(random_coordinate)
+      coordinates = [] if coordinates.length > ship.length
+    end
+    coordinates
+  end
+
 end
