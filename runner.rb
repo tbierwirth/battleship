a5826db06ddc705cd1504a83fc88aefccd9aa6cf
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-05-15 14:33:53 -0600

    Add structure for runner

--- runner.rb
+++ runner.rb
@@ -1,17 +1,26 @@
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
+require './lib/setup'
 
-setup = Setup.new
-@player_cruiser = Ship.new
+@setup = Setup.new
+@computer_cruiser = Ship.new("Cruiser", 3)
+@player_cruiser = Ship.new("Cruiser", 3)
+@computer_submarine = Ship.new("Submarine", 2)
+@player_submarine = Ship.new("Submarine", 2)
+
+
 
 puts "Welcome to Battleship"
 puts "Enter p to Play or q to Quit"
 input = gets.chomp
-if input == 'p'
-  # Create board for computer and place ships randomly
-  # Create bordd for player and ask for coordinates to place their ships
-  # Start the game/initialize the Turn
-elsif input == 'q'
-  # Stop the game
-end
+  if input == 'p'
+    # Create board for computer and place ships randomly
+    # @setup.render_boards
+    @setup.random_placement(@computer_cruiser)
+    @setup.random_placement(@comuter_submarine)
+    # Create board for player and ask for coordinates to place their ships
+    # Start the game/initialize the Turn
+  elsif input == 'q'
+    # Stop the game
+  end
